c02c43089f26f39de86033f4ca6b7d84c2569360
Prateek Pai <paiprateek123@gmail.com>
2022-08-05 07:14:26 +0530

    Demonstrate Async nature of JavaScript

--- Timer.js
+++ Timer.js
@@ -1,2 +1,14 @@
-console.log("HTML, CSS, Javascript and Ajax Server Calls")
+//UC1 Demonstrate Async nature of JavaScript
+function showTime(){
+    const date = new Date();
+    return date.getHours() + "Hrs:" + date.getMinutes() + "Mins: "+ date.getSeconds() + "Sec:";
+}
+
+function showSessionExpire(){
+    console.log("Activity: B: Your session expired at "+ showTime());
+}
+
+console.log("Activity- A: Triggering Activity B at"+ showTime());
+setTimeout(showSessionExpire, 5000);
+console.log("Activity-A : Triggerred-B at "+ showTime()+ "will excute after 5 second");
 
